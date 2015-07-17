ec5bb0b50ce772e6437484e5db6dacf3e9320e9a
Manuel <solipa@gmail.com>
2015-07-17 19:19:25 +0200

    Adding shared javascript library

---
+++ rsc/js/socialshare.js
@@ -0,0 +1,5 @@
+
+function open_window( api_url ) {
+	window.open(api_url,'social_share','width=700,height=500,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');
+	return false;
+}
